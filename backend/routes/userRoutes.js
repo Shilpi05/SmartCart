f1058ccf9a1d98af522dc994dd53d6008947c390
Shilpi05 <shilpi.kumari7605@gmail.com>
2026-03-20 23:38:24 +0530

    Added gitignore and removed env from tracking

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import {
+  createUser,
+  loginUser,
+  logoutCurrentUser,
+  getAllUsers
+} from "../controllers/userController.js";
+
+import { authenticate, authorizeAdmin } from "../middlewares/authMiddleWare.js";
+
+const router = express.Router();
+
+router.route("/").post(createUser).get(authenticate, authorizeAdmin, getAllUsers);
+router.post("/auth", loginUser);
+router.post("/logout", logoutCurrentUser);
+
+export default router;
